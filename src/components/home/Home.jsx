8ef92f85e6f91f68454694ce79b633ffd142c0e8
Roche001 <rocheharold13@gmail.com>
2022-05-05 16:09:43 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,6 +20,7 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      transition={400}
     >
       <section id="Home">
         <Swiper
