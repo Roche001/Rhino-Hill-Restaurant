85c5df77062222e5c214b58c2c0ad76ae339ab2d
Roche001 <rocheharold13@gmail.com>
2022-06-20 06:57:50 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -20,74 +20,73 @@ const Top = () => {
             </div>
           </div>
         </Navbar.Brand>
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#navbar-collapse-main"
-            >
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#"></a>
-          </div>
 
-          <div id="navbar-collapse-main" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <Link
-                to="/"
-                className="link"
-                onClick={() => setActiveNav("/")}
-                id={activeNav === "/" ? "active" : ""}
-              >
-                <li>Home</li>
-              </Link>
-              <Link
-                to="/Popular"
-                className="link"
-                onClick={() => setActiveNav("/Popular")}
-                id={activeNav === "/Popular" ? "active" : ""}
-              >
-                {" "}
-                <li>Popular</li>
-              </Link>
-              <Link
-                to="/Shop"
-                className="link"
-                onClick={() => setActiveNav("/Shop")}
-                id={activeNav === "/Shop" ? "active" : ""}
-              >
-                <li>Shop</li>
-              </Link>
-              <Link
-                to="/Reservation"
-                className="link"
-                onClick={() => setActiveNav("/Reservation")}
-                id={activeNav === "/Reservation" ? "active" : ""}
-              >
-                <li>Reservation</li>
-              </Link>
-              <Link
-                to="/Account"
-                className="link"
-                onClick={() => setActiveNav("/Account")}
-                id={activeNav === "/Account" ? "active" : ""}
-              >
-                <li>Account</li>
-              </Link>
-              <Link
-                to="/Contacts"
-                className="link"
-                onClick={() => setActiveNav("/Contacts")}
-                id={activeNav === "/Contacts" ? "active" : ""}
-              >
-                <li>Contacts</li>
-              </Link>
-            </ul>
-          </div>
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#navbar-collapse-main"
+          >
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#"></a>
+        </div>
+
+        <div id="navbar-collapse-main" className="collapse navbar-collapse">
+          <ul className="nav navbar-nav navbar-right">
+            <Link
+              to="/"
+              className="link"
+              onClick={() => setActiveNav("/")}
+              id={activeNav === "/" ? "active" : ""}
+            >
+              <li>Home</li>
+            </Link>
+            <Link
+              to="/Popular"
+              className="link"
+              onClick={() => setActiveNav("/Popular")}
+              id={activeNav === "/Popular" ? "active" : ""}
+            >
+              {" "}
+              <li>Popular</li>
+            </Link>
+            <Link
+              to="/Shop"
+              className="link"
+              onClick={() => setActiveNav("/Shop")}
+              id={activeNav === "/Shop" ? "active" : ""}
+            >
+              <li>Shop</li>
+            </Link>
+            <Link
+              to="/Reservation"
+              className="link"
+              onClick={() => setActiveNav("/Reservation")}
+              id={activeNav === "/Reservation" ? "active" : ""}
+            >
+              <li>Reservation</li>
+            </Link>
+            <Link
+              to="/Account"
+              className="link"
+              onClick={() => setActiveNav("/Account")}
+              id={activeNav === "/Account" ? "active" : ""}
+            >
+              <li>Account</li>
+            </Link>
+            <Link
+              to="/Contacts"
+              className="link"
+              onClick={() => setActiveNav("/Contacts")}
+              id={activeNav === "/Contacts" ? "active" : ""}
+            >
+              <li>Contacts</li>
+            </Link>
+          </ul>
         </div>
       </Container>
     </Navbar>
