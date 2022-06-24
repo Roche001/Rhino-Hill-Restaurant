206db909d32fd0f16f923ed2b45f5bf8320089c9
Roche001 <rocheharold13@gmail.com>
2022-06-24 19:21:51 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -89,49 +89,48 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section id="Restaurant" className="row">
-        <div className="restorans-img  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <div className="translate">
+      <section id="Restaurant" className="container-fluid">
+        <div className="restorans-img row">
+          <div className="translate col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
             {" "}
-            <img src="./assets/rest1.jpg" alt="r" />
+            <img className="d-block w-100" src="./assets/rest1.jpg" alt="r" />
             rhino-hill
           </div>
-          <div className="translate">
+          <div className="translate col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
             {" "}
-            <img src="./assets/rest2.jpg" alt="r" />
+            <img className="d-block w-100" src="./assets/rest2.jpg" alt="r" />
           </div>
-          <div className="translate">
+          <div className="translate col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
             {" "}
-            <img src="./assets/rest3.jpg" alt="r" />
+            <img className="d-block w-100" src="./assets/rest3.jpg" alt="r" />
           </div>
-          <div className="translate">
+          <div className="translate col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
             {" "}
-            <img src="./assets/rest4.jpg" alt="r" />
+            <img className="d-block w-100" src="./assets/rest4.jpg" alt="r" />
           </div>
-        </div>
-        <div className="restorans  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <h4>Our Restorāns</h4>
-          <h5>The Rhino Hill Restaurant </h5>{" "}
-          <p>
-            The Rhino Hill Restaurant has been built in the best traditions of
-            Mediterranean cuisine, bringing a sunny atmosphere to the with light
-            and calm colors, stone mosaic walls, southern ornaments and fresh
-            design elements. The restaurant’s menu combines Italian, Spanish,
-            French and Greek cuisine, complemented by a specially designed
-            Mediterranean wine list. The menu is designed by Jānis Sokolovskis,
-            the restaurant's chef and co-owner, winner of several international
-            culinary competitions. According to him, it combines the best
-            products in a way that is understandable to the gourmets of our{" "}
-            region, with the characteristic, familiar taste of the Riviera. The{" "}
-            restaurant serves a combination of the best products from today and{" "}
-            the world, which reveals the restaurant's usual as well as new
-            nuances of taste. The extensive wine list, with more than 450 items,
-            is created by the well-known sommelier and restaurant co-owner
-            Eduards Jakuševskis and combines wines from several countries and
-            regions.{" "}
-          </p>
         </div>
       </section>
+      <div className="restorans ">
+        <h4>Our Restorāns</h4>
+        <h5>The Rhino Hill Restaurant </h5>{" "}
+        <p>
+          The Rhino Hill Restaurant has been built in the best traditions of
+          Mediterranean cuisine, bringing a sunny atmosphere to the with light
+          and calm colors, stone mosaic walls, southern ornaments and fresh
+          design elements. The restaurant’s menu combines Italian, Spanish,
+          French and Greek cuisine, complemented by a specially designed
+          Mediterranean wine list. The menu is designed by Jānis Sokolovskis,
+          the restaurant's chef and co-owner, winner of several international
+          culinary competitions. According to him, it combines the best products
+          in a way that is understandable to the gourmets of our region, with
+          the characteristic, familiar taste of the Riviera. The restaurant
+          serves a combination of the best products from today and the world,
+          which reveals the restaurant's usual as well as new nuances of taste.
+          The extensive wine list, with more than 450 items, is created by the
+          well-known sommelier and restaurant co-owner Eduards Jakuševskis and
+          combines wines from several countries and regions.{" "}
+        </p>
+      </div>
       <section id="deals">
         <div className="overlay"></div>
         <div className="weekly-deals">
