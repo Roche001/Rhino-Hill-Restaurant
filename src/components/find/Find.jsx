7b8b04bbd357aa304350024f2f954407c4be9b55
Roche001 <rocheharold13@gmail.com>
2022-06-26 01:27:15 +0300

    update changes

--- src/components/find/Find.jsx
+++ src/components/find/Find.jsx
@@ -54,7 +54,7 @@ const Find = () => {
           </form>
         </div>
       </div>
-      <div className="last row">
+      {/* <div className="last row">
         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
           <h4>+8(876)09876543</h4>
         </div>
@@ -64,7 +64,7 @@ const Find = () => {
         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
           <h4>Rhinohillrestaurant@rhino.com</h4>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
