4445161e90715dd39da33fb1202d1c9e005c49e6
Roche001 <rocheharold13@gmail.com>
2022-06-27 08:46:57 +0300

    update changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import { AiOutlineCopyrightCircle } from "react-icons/ai";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const Footer = () => {
   return (
