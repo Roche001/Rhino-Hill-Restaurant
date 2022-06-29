15a8e167a9a1761b37f532dbf169b355e94373db
Roche001 <rocheharold13@gmail.com>
2022-06-29 21:35:50 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -391,52 +391,6 @@ const Home = () => {
           <FaQuoteLeft />
         </div>
       </section>
-
-      <div
-        id="carouselBasicExample"
-        className="carousel slide carousel-fade"
-        data-mdb-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp"
-              className="d-block w-100"
-              alt="Sunset Over the City"
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-
-          <div className="carousel-item">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp"
-              className="d-block w-100"
-              alt="Canyon at Nigh"
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-
-          <div className="carousel-item">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp"
-              className="d-block w-100"
-              alt="Cliff Above a Stormy Sea"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
