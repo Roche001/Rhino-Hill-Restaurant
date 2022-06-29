45b7b1b52d3de1a465b2c4dfdf263698c49e9154
Roche001 <rocheharold13@gmail.com>
2022-06-29 17:07:09 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -391,80 +391,74 @@ const Home = () => {
           <FaQuoteLeft />
         </div>
       </section>
+
       <div
-        id="carouselExampleDark"
-        class="carousel carousel-dark slide"
-        data-bs-ride="carousel"
+        id="carouselBasicExample"
+        className="carousel slide carousel-fade"
+        data-mdb-ride="carousel"
       >
         <div class="carousel-indicators">
           <button
             type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="0"
+            data-mdb-target="#carouselBasicExample"
+            data-mdb-slide-to="0"
             class="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="1"
+            data-mdb-target="#carouselBasicExample"
+            data-mdb-slide-to="1"
             aria-label="Slide 2"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="2"
+            data-mdb-target="#carouselBasicExample"
+            data-mdb-slide-to="2"
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
-            <img src="..." class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
+
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp"
+              className="d-block w-100"
+              alt="Sunset Over the City"
+            />
+            <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </div>
           </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="..." class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
+
+          <div className="carousel-item">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp"
+              className="d-block w-100"
+              alt="Canyon at Nigh"
+            />
+            <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
+
+          <div className="carousel-item">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp"
+              className="d-block w-100"
+              alt="Cliff Above a Stormy Sea"
+            />
+            <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>
-                Some representative placeholder content for the third slide.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
               </p>
             </div>
           </div>
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
     </section>
   );
