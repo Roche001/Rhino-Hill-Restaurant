7b4ff034da8070b00cd0c4db50fc65ed6a8bda06
Roche Harold <rocheharold13@gmail.com>
2022-08-30 08:43:58 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -16,17 +16,42 @@ const Home = () => {
     <section id="Home" className="container-fluid" data-aos="zoom-in-up">
       <Carousel>
         <Carousel.Item interval={1000} className="sized">
-          <img src="./assets/slider1.jpg" alt="Featured-House" />
+          <img
+            className="d-block w-100"
+            src="./assets/slider1.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500} className="sized">
-          <img src="./assets/slider2.jpg" alt="Featured-House" />
+          <img
+            className="d-block w-100"
+            src="./assets/slider2.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500} className="sized">
-          <img src="./assets/slider3.jpg" alt="Featured-House" />
+        <Carousel.Item className="sized">
+          <img
+            className="d-block w-100"
+            src="./assets/slider3.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
-      <section id="about">
+      <section className="about">
         <div className="operations">
           <div className="opening">
             <h3>OPENING HOURS </h3>
